10f6c5b34e14b255ba77c264531c2153aef7c6be
Himanshu Singh <Singhhimanshu33456@gmail.com>
2025-08-20 19:05:34 +0530

    template created, requirements added successfully

--- README.md
+++ README.md
@@ -3,3 +3,38 @@
 conda create -p plate python==3.10 -y
 
 conda activate /Users/himanshu/Downloads/DataScience/DA/Automatic-Number-Plate-Detection/plate
+
+create template.py
+
+AutomaticNumberPlateDetection/
+ ├── .github/workflows/.gitkeep
+ ├── src/AutomaticNumberPlateDetection/
+ │   ├── __init__.py
+ │   ├── data/
+ │   │   ├── __init__.py
+ │   │   └── dataloader.py
+ │   ├── components/
+ │   │   ├── __init__.py
+ │   │   ├── detection.py
+ │   │   └── ocr.py
+ │   ├── utils/
+ │   │   ├── __init__.py
+ │   │   └── common.py
+ │   ├── config/
+ │   │   ├── __init__.py
+ │   │   └── configuration.py
+ │   ├── pipeline/
+ │   │   ├── __init__.py
+ │   │   ├── training_pipeline.py
+ │   │   └── inference_pipeline.py
+ │   ├── entity/__init__.py
+ │   └── constants/__init__.py
+ ├── config/config.yaml
+ ├── dvc.yaml
+ ├── params.
+
+
+create setup.py
+
+pip install -r requirements.txt
+
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Automatic-Number-Plate-Detection"
+AUTHOR_USER_NAME = "Himanshusinghdev1"
+SRC_REPO = "AutomaticNumberPlateDetection"
+AUTHOR_EMAIL = "singhhimanshu33456@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for Automatic Number Plate Detection using YOLOv5 and EasyOCR",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "opencv-python",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+        "PyYAML",
+        "seaborn",
+        "easyocr"
+    ],
+    python_requires=">=3.8",
+)
